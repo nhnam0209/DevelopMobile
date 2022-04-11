cf63edfa292336f10022df658ddd6dbb02e10676
nhnam0209 <93809958+nhnam0209@users.noreply.github.com>
2022-04-11 14:23:15 +0700

    Update navigation bar

--- Week_5/app/src/main/java/com/example/week_5/MainActivity.java
+++ Week_5/app/src/main/java/com/example/week_5/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.week_5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -12,15 +21,59 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView rvCoffee;
     List<Coffee> lstCoffee;
     CoffeeAdapter adapter;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.tb_toolbar);
+        toolbar.setTitle("The Coffee Land");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         rvCoffee = findViewById(R.id.rvCoffee);
         lstCoffee = Coffee.init();
         rvCoffee.setLayoutManager(new LinearLayoutManager(this));
         adapter = new CoffeeAdapter(this, lstCoffee);
         rvCoffee.setAdapter(adapter);
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()){
+            case R.id.mycart:
+                break;
+            case R.id.call: break;
+            case R.id.mail: break;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+
 }
